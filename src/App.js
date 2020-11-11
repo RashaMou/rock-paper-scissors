1c458775044bfc5250714aafe0a6adf46603cabd
Rasha Moumneh <rasha@fastmail.com>
2020-11-11 18:30:07 -0500

    Revert to sass and add header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import {ReactComponent as Logo} from './assets/images/logo.svg';
+import './styles/index.scss'
 
 function App() {
   return (
-    <div className="App">
-      <Logo />
+    <div className="App" >
+      <div className="header-container">
+        <Logo />
+        <div className="score-container">
+          <h2 className="score-text">SCORE</h2>
+          <h2 className="score">12</h2>
+        </div>
+      </div>
     </div>
   );
 }
